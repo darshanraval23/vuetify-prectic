439075f38127680df9b0b5aff1128ca125866574
darshan-raval-codlink <dr@codelinkinfotech.com>
2022-10-15 09:54:36 +0530

    init commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+// import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
+
+// export default new vuetify({
+//   icons: {
+//     iconfont: 'mdi', // default - only for display purposes
+//   },
+// })
+  
