9a5584556b835ccffb6335d49bc0007df27dc3b7
darshan-raval-codlink <dr@codelinkinfotech.com>
2022-10-17 18:31:22 +0530

    formand form validation done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,12 +13,13 @@
         <v-divider></v-divider>
 
         <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home" ></v-list-item>
           <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
         </v-list>
       </v-navigation-drawer>
       <v-main style="height: 250px">
         <div class="d-flex justify-center align-center h-100">
+        <h1>main</h1>
           <v-btn
             color="primary"
             @click.stop="drawer = !drawer"
@@ -34,7 +35,9 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      iscomponent: 'home'
+
     }
   },
 }
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,20 +1,3 @@
-<script>
-import {
-    ref,
-    onMounted
-} from "vue"
-// import { mdiAccount } from '@mdi/js'
-
-let name = ref('')
-// let navTroggle = ref('false')
-
-// function changename(){
-//     name.value = 'darshan'
-//     }
-onMounted(() => {
-    name.value = 'hallo'
-})
-</script>
 <template>
 <v-app>
     <!-- vartical navigation bar -->
@@ -22,8 +5,11 @@ onMounted(() => {
         <v-layout>
             <v-navigation-drawer app v-model="drawer">
                 <!-- item -->
-                <v-list-item title="darshan" link>
-                    <v-list-tite>Vuetify</v-list-tite>
+                <v-list-item>
+                    <v-list-item-avatar class="pa-0">
+                        <v-img src="@/assets/logo.png"></v-img>
+                    </v-list-item-avatar>
+                    <v-list-item-title>Vuetify 2.6</v-list-item-title>
                 </v-list-item>
                 <!-- delimiter -->
                 <v-divider></v-divider>
@@ -56,12 +42,65 @@ onMounted(() => {
     <!-- main content -->
     <v-main app>
         <v-container>
-            <v-container>
-                <v-col>
+            <v-row no-gutters>
+                <v-col cols="12" sm="6">
+                    <v-hover v-slot="{ hover }">
+                        <v-card :elevation="hover ? 10 :6" color="#F6F7F9" class="ma-2 pa-6">
+                            <v-form>
+                                <div class="text-h5 text-center">
+                                    Form
+                                </div>
+                                <v-text-field v-model="formdata.name" :counter="10" clearable placeholder="Enter Your Name" label="Name" :rules="rules.name" required>
+                                    <v-icon slot="prepend">mdi-account</v-icon>
+                                </v-text-field>
+                                <v-text-field label="Email" :rules="rules.email" required>
+                                    <v-icon slot="prepend">mdi-email</v-icon>
+                                </v-text-field>
+                                {{selectdproject}}
+                                <v-select v-model="selectdproject" :items="formdata.products" item-text="project" return-object single-line item-value="projectcode" label="Select Project" required>
 
+                                </v-select>
+
+                                <p>Select Your Hobby</p>
+                                <v-checkbox label="Reading" value="reading" v-model="formdata.hobby"></v-checkbox>
+                                <v-checkbox label="Writing" value="Writing" v-model="formdata.hobby"></v-checkbox>
+
+                                <v-file-input label="Select Image" prepend-icon="mdi-cloud-upload" show-size accept="image/*,png,jpg,jpeg" multiple></v-file-input>
+
+                                <v-radio-group label="Choose Your Gender" column>
+                                    <v-radio label="Male" value="male"></v-radio>
+                                    <v-radio label="Female" value="female"></v-radio>
+                                </v-radio-group>
+                                <v-textarea label="address" auto-grow outlined raw-height="15" counter="50" prepend-icon="mdi-map-marker">
+                                </v-textarea>
+
+                                {{isConform}}
+                                <v-switch v-model="isConform" label="Switch On/Off"></v-switch>
+
+                                <!-- <div class="row" label="Allow permiossion">
+                                <p>Allow permiossion</p>
+                               <v-switch v-model="ispermisstion" label="Edit" value="edit"></v-switch>
+                               <v-switch v-model="ispermisstion" label="Add" value="add"></v-switch>
+                               <v-switch v-model="ispermisstion" label="Delate" value="delate"></v-switch>
+                               <v-switch v-model="ispermisstion" label="view" value="view"></v-switch>
+                                </div> -->
+                                <!-- @input="$v.name.$touch()" @blur="$v.name.$touch()"" -->
+                                <v-btn @click="save" color="success">
+                                    Submit
+                                </v-btn>
+                            </v-form>
+                        </v-card>
+                    </v-hover>
                 </v-col>
-            </v-container>
-
+                <v-col cols="12" sm="6">
+                    <v-hover v-slot="{hover}">
+                        <v-card :elevation="hover ? 10 :6" color="#F6F7F9" class="ma-2 pa-6">
+                            <p class="text-h5 text-center">Autocomplete</p>
+                            <v-autocomplete label="Select Your state" v-model="isSelectstate" :items="states" persistent-hint prepend-icon="mdi-city"></v-autocomplete>
+                        </v-card>
+                    </v-hover>
+                </v-col>
+            </v-row>
         </v-container>
     </v-main>
     <!-- end main content -->
@@ -75,17 +114,73 @@ onMounted(() => {
 
 <script>
 export default {
-    setup() {
-
-    },
+    name: 'HeaderComponent',
     data() {
         return {
             drawer: true,
-             items: [
-        { title: 'Home', icon: 'mdi-view-dashboard' },
-        { title: 'About', icon: 'mdi-forum' },
-      ],
+            items: [{
+                    title: 'Home',
+                    icon: 'mdi-view-dashboard'
+                },
+                {
+                    title: 'Forms',
+                    icon: 'mdi-forum'
+                },
+            ],
+            product: [
+                'item1', 'item2'
+            ],
+            states: [
+                'surat', 'ahmedabad', 'rajkot'
+            ],
+            isSelectstate: '',
+            isConform: '',
+            ispermisstion: [],
+            rules: {
+                name: [
+                    value => !!value || 'name is required.',
+                    value => value.length <= 20 || 'Max 20 characters'
+                ],
+                email: [
+                    value => !!value || 'email is required.',
+                    value => (value || '').length <= 20 || 'Max 20 characters',
+                    value => {
+                        const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                        return pattern.test(value) || 'Invalid e-mail.'
+                    },
+                ],
+                
+            },
+            formdata: {
+                name: '',
+                gender: '',
+                products: [{
+                        project: 'abc',
+                        projectcode: '1'
+                    },
+                    {
+                        project: 'xyz',
+                        projectcode: '2'
+                    },
+                    {
+                        project: 'ABC',
+                        projectcode: '3'
+                    },
+                ],
+                selectdproject: '',
+                hobby: ''
+
+            }
         }
     },
+    methods: {
+        save() {
+            console.log(this.formdata)
+        },
+        emailError() {
+
+        }
+
+    }
 }
 </script>
